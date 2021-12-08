bf4712c5b6001d826d584df8222fa1d782fd7b54
Kishan B <bkishan@thoughtworks.com>
2021-12-08 22:04:39 +0530

    Update setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 My dotfiles
+
+
+### Setting up a new mac machine
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/kishaningithub/dotfiles/main/setup-machine.sh)"
+```
--- setup-machine.sh
+++ setup-machine.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
-# Install homebrew
+echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing ohmyzsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Cloning dotfiles repo..."
+git clone https://github.com/kishaningithub/dotfiles.git ~/dotfiles
+
+echo "Installing all the dependencies..."
+brew bundle install --file=~/dotfiles/.Brewfile
